4022a732957a7b60e047182429a4d233a23a22c8
Luisa Mora <luisamora895@gmail.com>
2023-06-19 11:34:11 -0400

    motor de vistas

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
+//const ejs=require('ejs');
 const userRoute = require('./routes/Roouter');
  
- app.set("views engine",'ejs');
+//app.set('views', path.join(__dirname, 'views'));
+//app.engine('html', require('ejs').renderFile);
+ app.set('view engine','ejs');
+//app.set('views',__dirname+'/views');
 
- app.use('/',userRoute);
+app.use('/',userRoute);
+/*
+app.get('/', function(req, res) {
+   res.render('home');
+ });
 
+ app.get('/about', function(req, res) {
+   res.render('about');
+ });
+*/
  app.listen(3000,()=>{
     console.log("hoaaaa port 3000")
  });
--- routes/Roouter.js
+++ routes/Roouter.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
 //const controller=require('../controllers/UserControllers')
-const conexion =require('../database/db');
+//const conexion =require('../database/db');
 
 router.get('/',(req,res)=>{
-    conexion.query('SELECT * FROM users',(error,results)=>
+    res.render('home')
+/*     conexion.query('SELECT * FROM users',(error,results)=>
     {
+        throw error;
         if(error){
-            throw error;
         }
         else{
             res.send(results);
         }
-    })
+    }) */
 });
 
 
